bb93eaa9d7c75621545976e2ad826b8089cfc255
Thompson Plyler <nealok.g@gmail.com>
2018-11-01 22:43:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(x)
+
+  array = (1..x).to_a
+
+  for y in array do
+    if (x % y) == 0
+      puts "#{x} is not divisible by #{y}"
+    else
+      puts "This is not a prime number since this is divisible by #{y}"
+      break
+    end
+  end
+end
