93ac45ab33130ea4b289e1cb4718d78538dea9dd
Thompson Plyler <nealok.g@gmail.com>
2018-11-01 22:52:35 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(x)
+  
+  if x < 2 
+    return false
+  else 
+    (2...x).each do |y|
+      if x % y == 0 
+        return false
+      end
+  end
+end
+  
+  true
+
+end
