b5c5cc963d79a3003d6743b75b10883fcd2fb625
Thompson Plyler <nealok.g@gmail.com>
2018-11-01 22:46:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,8 @@
 def prime?(x)
-
-  array = (1..x).to_a
-
-  for y in array do
-    if (x % y) == 0
-      puts "#{x} is not divisible by #{y}"
-    else
-      puts "This is not a prime number since this is divisible by #{y}"
-      break
+  (2...x).each do |y|
+    if x % y == 0 
+      return false
     end
   end
+  true
 end
